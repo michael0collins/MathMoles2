8d5a502215e5c97d03a554f7ac74bb8ba2ecd59e
Michael Collins <michaelmartincollins@gmail.com>
2019-07-12 23:49:36 -0700

    Formatted code into sections and fixed the jump to be faster and more responsive.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,34 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Player Variables")]
+    //Public Fields
+    [Header("Player Movement")]
     public float groundedSpeed = 3.0f;
-    public float airSpeed = 2.0f;
-    public float jumpForce = 4.5f;
-    public float attackForce = 100f;
+    public float airSpeed = 1.0f;
+    public float fallMultiplier = 3.5f;
+    public float lowJumpMultiplier = 2.5f;
+    public float jumpForce = 9.0f;
+    public bool jumpButtonIsPressed = false;
+    public bool isGrounded { get; private set; }
+    public float currentSpeed { get; private set; } = 0f;
 
+    [Header("Player Interaction")]
+    public float attackForce = 100f;
     public float attackDistance = 2f;
     public float attackFieldSize = 1f;
 
+    //Buttons for mobile use.
+    private Button jumpButton;
+    private Button axeSwingButton;
+
+
+    //Animation
+    [HideInInspector]
+    public float AnimationSpeed = 0f;
     public PlayerAnimationController playerAnimationController;
+
+    //Network
+    public NetworkPlayer networkPlayer { get; private set; }
 
     [Header("Debug")]
     public bool debugMode = false;
 
+    //Private Fields
     private Rigidbody rb;
     private Joystick joyStick;
-    public NetworkPlayer networkPlayer { get; private set; }
-    public bool isGrounded { get; private set; }
-    public float currentSpeed { get; private set; } = 0f;
-
-    [HideInInspector]
-    public float AnimationSpeed = 0f;
 
     private void Awake()
     {
+        #if UNITY_IPHONE || UNITY_ANDROID
+            PopulateMobileActionButtons
+        #endif
+
         networkPlayer = GetComponent<NetworkPlayer>();
         joyStick = FindObjectOfType<Joystick>();
         rb = GetComponent<Rigidbody>();
@@ -48,6 +66,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    #region InputHandling
     private void ProcessUserInput(bool isMobile)
     {
         Vector2 playerInput;
@@ -60,7 +79,10 @@ public class PlayerController : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Space))
                 Jump();
-        } else
+
+            JumpForceModifier();
+        }
+        else
             playerInput = new Vector2(joyStick.Horizontal, joyStick.Vertical);
 
         playerInput.Normalize();
@@ -80,12 +102,42 @@ public class PlayerController : MonoBehaviour
         Vector3 rotVector = velocity;
         rotVector.y = 0;
         transform.forward = rotVector;
-    }     
+    }
 
-    bool RayCastCollisionCheck(Vector3 position, Vector3 direction, float distance, string layer)
+    private void PopulateMobileActionButtons()
     {
-        RaycastHit hit;
-        return Physics.Raycast(position, direction, out hit, distance, 1 << LayerMask.NameToLayer(layer));
+        if (GameObject.FindGameObjectWithTag("JumpButton").GetComponent<Button>() != null)
+            jumpButton = GameObject.FindGameObjectWithTag("JumpButton").GetComponent<Button>();
+        else
+            Debug.Log("No button found with tag 'JumpButton'");
+
+        if (GameObject.FindGameObjectWithTag("").GetComponent<Button>() != null)
+            axeSwingButton = GameObject.FindGameObjectWithTag("SwingAxeButton").GetComponent<Button>();
+        else
+            Debug.Log("No button found with tag 'SwingAxeButton'");
+
+        jumpButton.onClick.AddListener(Jump);
+        //axeSwingButton.onClick.AddListener(Jump); //Replace attack with swing axe, choose based on context what happens.
+    }
+    #endregion
+
+    #region Movement&Spatial
+    public void Jump()
+    {
+        if (isGrounded)
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + jumpForce, rb.velocity.z);
+    }
+
+    private void JumpForceModifier()
+    {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
 
     public void GetKnockedBack(Vector3 position)
@@ -93,27 +145,33 @@ public class PlayerController : MonoBehaviour
         rb.AddForce((transform.position - position) * attackForce);
     }
 
-    public void Jump()
+    bool RayCastCollisionCheck(Vector3 position, Vector3 direction, float distance, string layer)
     {
-        if (isGrounded)
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y + jumpForce, rb.velocity.z);
+        RaycastHit hit;
+        return Physics.Raycast(position, direction, out hit, distance, 1 << LayerMask.NameToLayer(layer));
     }
+    #endregion
 
+    #region Attack&Interaction
     public void Attack()
     {
         if (!isGrounded)
             return;
 
         playerAnimationController.AttackTrigger();
-        if (networkPlayer.isLocal) {
+        if (networkPlayer.isLocal)
+        {
             RaycastHit hit;
             if (Physics.SphereCast(transform.position, attackFieldSize, transform.forward, out hit, attackDistance))
             {
                 if (hit.transform.gameObject.tag == "Player")
                     networkPlayer.SendHitData(hit.transform.GetComponent<NetworkPlayer>(), hit.point);
-            } else {
+            }
+            else
+            {
                 networkPlayer.SendFailedHitData();
             }
         }
     }
+    #endregion
 }
