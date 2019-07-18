5da0d4c0f09010dea3e62687f87e97adaf9cd491
Michael Collins <michaelmartincollins@gmail.com>
2019-07-17 17:32:47 -0700

    Added placeholder numbers and operators for the challenge question, added challenge question object populator.

--- Assets/Scripts/ChallengeQuestions/QuestionObjectPopulator.cs
+++ Assets/Scripts/ChallengeQuestions/QuestionObjectPopulator.cs
@@ -4,5 +4,65 @@ using UnityEngine;
 
 public class QuestionObjectPopulator : MonoBehaviour
 {
-    //function to populate container with models based off string data.
+    private float letterSpacing = 1.25f;
+    private const string path = "Symbols";
+
+    private void Start()
+    {
+        print(RetrieveSymbols("2@*@12"));
+    }
+
+    public Symbol[] RetrieveSymbols(string symbols)
+    {
+        //Symbols must be sperated by '@' to seperate values with more than one character.
+        //Create an array for each individual symbol(s)
+        string[] processedSymbols = symbols.Split('@');
+
+        //Create an array for the newly added symbols.
+        Symbol[] retrievedSymbols = new Symbol[processedSymbols.Length];
+
+        //Keep track of spacing.
+        int spacingCounter = 0;
+
+        //For each symbol in the array, get the game objects and add them.
+        for (int i = 0; i < processedSymbols.Length; i++)
+        {
+            for (int j = 0; j < processedSymbols[i].Length; j++)
+            {
+                string character = processedSymbols[i].Substring(j, 1);
+                print(path + "/" + processedSymbols[i].Substring(j, 1));
+                Symbol symbolToInstantiate;
+                if(character == "*" || character == "/" || character == "+" || character == "-")
+                {
+                    string characterToRetrieve = "";
+                    switch (character)
+                    {
+                        case "*":
+                            characterToRetrieve = "Multiply";
+                            break;
+                        case "/":
+                            characterToRetrieve = "Divide";
+                            break;
+                        case "+":
+                            characterToRetrieve = "Add";
+                            break;
+                        case "-":
+                            characterToRetrieve = "Subtract";
+                            break;
+                        default:
+                            break;
+                    }
+                    symbolToInstantiate = Resources.Load<Symbol>(path + "/" + characterToRetrieve) as Symbol;
+                } else
+                {
+                    symbolToInstantiate = Resources.Load<Symbol>(path + "/" + character) as Symbol;
+                }
+                Symbol symbolClone = Instantiate(symbolToInstantiate, transform);
+                symbolClone.transform.position = new Vector3(transform.position.x + (spacingCounter * letterSpacing), transform.position.y, transform.position.z);
+                spacingCounter++;
+            }
+        }
+        
+        return retrievedSymbols;
+    }
 }
