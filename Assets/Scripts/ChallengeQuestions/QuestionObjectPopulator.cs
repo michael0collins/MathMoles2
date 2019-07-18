8bc5379d915f27f22e43d71c93df08d147bfc7dc
TylkoKuba <kubapilasiewicz60@gmail.com>
2019-07-18 15:33:49 +0200

    Refractored some code and cleaned up the project.

--- Assets/Scripts/ChallengeQuestions/QuestionObjectPopulator.cs
+++ Assets/Scripts/ChallengeQuestions/QuestionObjectPopulator.cs
@@ -1,71 +1,67 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class QuestionObjectPopulator : MonoBehaviour
 {
-    private float letterSpacing = 1.25f;
-    private const string path = "Symbols";
+    [Header("Visuals")]
+    public GameObject symbolPrefab;
 
-    public Symbol[] PopulateContainerWithSymbols(string symbols)
+    [Header("Variables")]
+    public float letterSpacing = 1.25f;
+
+    private void OnEnable()
     {
+        NetworkManager.NewQuestion += OnNewQuestion;
+    }
+
+    private void OnDisable()
+    {
+        NetworkManager.NewQuestion -= OnNewQuestion;
+    }
+
+    private void OnNewQuestion(string question, string[] answers)
+    {
+        PopulateContainerWithSymbols("2@*@12");
+        //throw new NotImplementedException();
+    }
+
+    public List<Symbol> PopulateContainerWithSymbols(string symbols)
+    {
+        Debug.Log($"Instantiating symbols...");
         //Symbols must be sperated by '@' to seperate values with more than one character.
         //Create an array for each individual symbol(s)
         string[] processedSymbols = symbols.Split('@');
 
         //Create an array for the newly added symbols.
-        Symbol[] retrievedSymbols = new Symbol[processedSymbols.Length];
+        List<Symbol> retrievedSymbols = new List<Symbol>();
 
         //Keep track of spacing.
         int spacingCounter = 0;
 
         //For each symbol in the array, get the game objects and add them.
         for (int i = 0; i < processedSymbols.Length; i++)
-        {
             for (int j = 0; j < processedSymbols[i].Length; j++)
             {
                 string character = processedSymbols[i].Substring(j, 1);
-                print(path + "/" + processedSymbols[i].Substring(j, 1));
-                Symbol symbolToInstantiate;
-                if(character == "*" || character == "/" || character == "+" || character == "-")
-                {
-                    string characterToRetrieve = "";
-                    switch (character)
-                    {
-                        case "*":
-                            characterToRetrieve = "Multiply";
-                            break;
-                        case "/":
-                            characterToRetrieve = "Divide";
-                            break;
-                        case "+":
-                            characterToRetrieve = "Add";
-                            break;
-                        case "-":
-                            characterToRetrieve = "Subtract";
-                            break;
-                        default:
-                            break;
-                    }
-                    symbolToInstantiate = Resources.Load<Symbol>(path + "/" + characterToRetrieve) as Symbol;
-                } else
-                {
-                    symbolToInstantiate = Resources.Load<Symbol>(path + "/" + character) as Symbol;
-                }
-                Symbol symbolClone = Instantiate(symbolToInstantiate, transform);
-                symbolClone.transform.position = new Vector3(transform.position.x + (spacingCounter * letterSpacing), transform.position.y, transform.position.z);
+                GameObject symbolObject = Instantiate(symbolPrefab, transform);
+                symbolObject.transform.position = new Vector3(transform.position.x + (spacingCounter * letterSpacing), transform.position.y, transform.position.z);
+                symbolObject.GetComponentInChildren<TextMeshPro>().text = character;
+                retrievedSymbols.Add(symbolObject.GetComponent<Symbol>());
+
                 spacingCounter++;
             }
-        }
-        
+
+        Debug.Log($"Done instantiating {spacingCounter} symbols!");
+
         return retrievedSymbols;
     }
 
     public void ClearObjects()
     {
         foreach(Symbol s in GetComponentsInChildren<Symbol>())
-        {
             s.TriggerOnClear();
-        }
     }
 }
