a76aa014c4a1ab87c05e2c20a553fc313ebb0292
Michael Collins <michaelmartincollins@gmail.com>
2019-07-27 23:47:29 -0700

    Added goal object

---
+++ Assets/Scripts/GoalObject/GoalObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalObject : MonoBehaviour
+{
+    public int hitsToCollect = 5;
+    public string answer = "";
+
+    public void RecieveHit()
+    {
+        hitsToCollect--;
+        if(hitsToCollect == 0)
+        {
+            ReportObjectToServer();
+            //Display the answer that was in this goal object.
+            //Give feedback as win
+            //Give feedback as loss
+        }
+        else
+        {
+            //Reduce size / show effect shrinking effect.
+        }
+    }
+
+    private void ReportObjectToServer()
+    {
+        //Check with the server to see if this is the correct object.
+    }
+}
