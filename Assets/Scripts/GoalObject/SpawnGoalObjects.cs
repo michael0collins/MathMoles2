9eb9ea97d0789502709ac007f53848326202504c
Michael Collins <michaelmartincollins@gmail.com>
2019-07-30 02:02:23 -0700

    Fixed goal objects to store the answer data

--- Assets/Scripts/GoalObject/SpawnGoalObjects.cs
+++ Assets/Scripts/GoalObject/SpawnGoalObjects.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 
 public class SpawnGoalObjects : MonoBehaviour
 {
-    public float spacing = 1.0f;
     public GoalObject goalObjectPrefab;
-
-    private NetworkManager nManager;
+    public float spacing = 1.0f;
 
     private void Start()
     {
-        nManager = FindObjectOfType<NetworkManager>();
-
         SpawnObjects();
     }
 
     void SpawnObjects()
     {
-        string[] answers = new string[] { "10", "12", "5", "7" }; //REPLACE WITH SERVER ANSWER VALUES.
-        for (int i = 0; i < answers.Length; i++)
+        for (int i = 0; i < NetworkManager.Answers.Length; i++)
         {
             Vector3 location = new Vector3(transform.position.x + (i * spacing), transform.position.y, transform.position.z);
             GoalObject goalObjectClone = Instantiate(goalObjectPrefab, location, Quaternion.identity) as GoalObject;
-            goalObjectClone.answer = answers[i];
+            goalObjectClone.answer = NetworkManager.Answers[i];
         }
     }
 }
