966339cbc50b989706e6756479e6d54c48414be1
Michael Collins <michaelmartincollins@gmail.com>
2019-08-14 18:10:48 -0700

    added first pass of new models as well as music tracks

--- Assets/Scripts/GoalObject/SpawnGoalObjects.cs
+++ Assets/Scripts/GoalObject/SpawnGoalObjects.cs
@@ -17,7 +17,7 @@ public class SpawnGoalObjects : MonoBehaviour
         for (int i = 0; i < NetworkManager.Answers.Length; i++)
         {
             Vector3 location = new Vector3(transform.position.x + (i * spacing), transform.position.y, transform.position.z);
-            GoalObject goalObjectClone = Instantiate(goalObjectPrefab, location, Quaternion.identity) as GoalObject;
+            GoalObject goalObjectClone = Instantiate(goalObjectPrefab, location, new Quaternion(0, Random.Range(0,360), 0,0)) as GoalObject;
             goalObjectClone.answer = NetworkManager.Answers[i];
             goalObjectClone.goalIndex = i;
         }
